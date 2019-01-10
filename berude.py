8b7fbcc929510c8d893ac54fb3790f9067ba2a83
Stephen Stagg <stestagg@gmail.com>
2019-01-10 19:48:47 +0000

    Iniitial

---
+++ berude.py
@@ -0,0 +1,19 @@
+import sys
+
+def find_rude_rhyme(word):
+   # takes a word and finds rude rhyme, returns rude subsititue or None if none found
+   
+
+
+
+def main():
+    sonnett = load_sonnett()
+    rude_version = make_rude(sonnett)
+
+    print(rude_version)
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
